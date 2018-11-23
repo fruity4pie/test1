438926c43db3e55e4f9055a5ee85d26837807ef3
fruity4pie <fruity4pie@gmail.com>
2018-11-23 03:28:00 +0200

    added axios for IE11

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import React, { Component } from 'react';
+import React, {
+	Component
+} from 'react';
 import Card from './components/Card';
 import 'core-js/es6/map';
 import 'core-js/es6/set';
+import 'core-js/fn/promise';
+import axios from 'axios';
 
 
 class App extends Component {
@@ -11,29 +15,47 @@ class App extends Component {
 	}
 
 	componentDidMount() {
-		fetch('http://demo4452328.mockable.io/properties')
-			.then(res => res.json())
-			.then(data => {
+		axios.get('http://demo4452328.mockable.io/properties')
+			.then(res => {
+				const data = res.data;
 				this.setState({
 					isLoaded: true,
 					data,
 				})
 			})
+
 	}
 
 	render() {
 		const arr = this.state.data.data;
 
-		return (
-			<div className="container">
-				<div className="row">
-					{this.state.isLoaded === true ? arr.map(item => (
-						<Card key={item.id} src={item.images[0]} adds={item.full_address} val={item.price} sq={item.area}/>
-					)) : ''}
-				</div>
-			</div>
+		return ( <
+			div className = "container" >
+			<
+			div className = "row" > {
+				this.state.isLoaded === true ? arr.map(item => ( <
+					Card key = {
+						item.id
+					}
+					src = {
+						item.images[0]
+					}
+					adds = {
+						item.full_address
+					}
+					val = {
+						item.price
+					}
+					sq = {
+						item.area
+					}
+					/>
+				)) : ''
+			} <
+			/div> < /
+			div >
 		);
-  	}
+	}
 }
 
 export default App;
